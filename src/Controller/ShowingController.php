8ca01bda9955d106e60469c883c3545df587150d
isma91 <ismaydogmus@gmail.com>
2020-12-16 18:28:57 +0100

    create a showing controller

---
+++ src/Controller/ShowingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\ShowingService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/showing")
+ *
+ * Class ShowingController
+ * @package App\Controller
+ */
+class ShowingController extends AbstractController
+{
+    /**
+     * @Route("/", name="listMovie")
+     * @Template("Showing/index.html.twig")
+     * @param ShowingService $showingService
+     * @return array
+     */
+    public function index(ShowingService $showingService): array
+    {
+        $showings = $showingService->getAllShowings();
+        return ["showings" => $showings];
+    }
+}
